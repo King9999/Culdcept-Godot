9ff2dc904ea7e6dbfb05d5f0365e0651cb0b3d7b
King9999 <mikemurray056@gmail.com>
2026-01-08 13:50:11 -0500

    Added groups to organize resources parameters

--- Scripts/card_data.gd
+++ Scripts/card_data.gd
@@ -11,14 +11,17 @@ enum CardType { MINION, SPELL, SCROLL, EQUIPMENT, ENCHANTMENT }
 @export var card_type: CardType
 @export var card_name: String
 @export var flavour_text: String 	#can be anything, such as lore
-@export var card_cost: int			#base cost for all cards. Does not include land or card costs.
+
+@export_group("Card Costs")
+@export var magic_cost: int			#base cost for all cards. Does not include land or card costs.
 @export var land_cost_fire: int
 @export var land_cost_earth: int	#amount of lands a player needs to own to play a card.
 @export var land_cost_water: int
 @export var land_cost_air: int
-@export var card_cost_card: int		#amount of cards that need to be discarded to play a card
+@export var card_cost: int		#amount of cards that need to be discarded to play a card
 
 #conditional card effects
+@export_group("Card Effect Triggers", "card_effect_")
 @export var card_effect_start_of_combat: CardEffect
 @export var card_effect_end_of_combat: CardEffect
 @export var card_effect_start_of_player_turn: CardEffect
--- Scripts/card_minion.gd
+++ Scripts/card_minion.gd
@@ -11,18 +11,21 @@ enum MinionElement { FIRE, WATER, EARTH, AIR, NEUTRAL }
 @export var attack_power: float
 
 #critical vs element. Critical damage does 50% (1.5x) more damage against an element.
+@export_group("Criticals", "critical_hit_")
 @export var critical_hit_fire: bool
 @export var critical_hit_water: bool
 @export var critical_hit_earth: bool
 @export var critical_hit_air: bool
 
 #nullify element. If true, minion does not take any damage from the element and cannot be targeted.
+@export_group("Nullify Element", "nullify_")
 @export var nullify_fire: bool
 @export var nullify_water: bool
 @export var nullify_earth: bool
 @export var nullify_air: bool
 
 #pierce element. If true, minion bypasses land defense vs element.
+@export_group("Pierce Element", "pierce_")
 @export var pierce_fire: bool
 @export var pierce_water: bool
 @export var pierce_earth: bool
